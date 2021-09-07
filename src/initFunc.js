1f541a83a0aa921b028c997edc5d281bbde77aaf
Moin-A <moinqa6@gmail.com>
2021-09-07 17:20:47 +0530

    image url edit

--- src/initFunc.js
+++ src/initFunc.js
@@ -91,7 +91,7 @@ export function popultatvehiclelist(classname, data) {
               max_distance: current.max_distance,
               total_no: current.total_no,
               speed: current.speed,
-            })} class="buttons cursor-pointer fill-mode focus:animate-spin-slow bg-green-400 rounded-lg p-2 hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
+            })} class="buttons disabled:cursor-not-allowed cursor-pointer fill-mode focus:animate-spin-slow bg-green-400 rounded-lg p-2 hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
             SELECT
             </button>         
             </div>
